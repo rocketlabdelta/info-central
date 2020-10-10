898eed08fa1c1c015b4f9964dfda18cf2f5b4551
Dan Horst <danhorst@gmail.com>
2020-10-09 20:08:00 -0400

    Strip leading lines of boilerplate
    The white space stripping is not working right though

--- _archive/convert_html.rb
+++ _archive/convert_html.rb
@@ -21,6 +21,20 @@ SITE_CATEGORIES = [
   'welcome'
 ].freeze
 
+LEADING_LINES = {
+  'construction' => 90,
+  'design'       => 90,
+  'electronics'  => 64,
+  'finishing'    => 59,
+  'propulsion'   => 73,
+  'recovery'     => 80,
+  'regulation'   => 61,
+  'research'     => 58,
+  'resources'    => 59,
+  'support'      => 63,
+  'welcome'      => 70
+}.freeze
+
 working_directory = File.expand_path(File.dirname(__FILE__))
 logger.debug(working_directory)
 target_directory = File.expand_path(File.join(working_directory, '..'))
@@ -63,6 +77,19 @@ def strip_tags(html)
   )
 end
 
+def strip_leading_lines(file, category)
+  return unless category
+  cmd = "sed -i '1,#{LEADING_LINES[category]}d' #{file}"
+  logger.debug(cmd)
+  `#{cmd}`
+end
+
+def strip_trailing_white_space(file)
+  cmd = "sed --in-place 's/[[:space:]]\+$//' #{file}"
+  logger.debug(cmd)
+  `#{cmd}`
+end
+
 files.each do |input_file|
   logger.debug("Reading from #{input_file}")
   raw_html = File.read(input_file, encoding: 'iso-8859-1')
@@ -70,4 +97,10 @@ files.each do |input_file|
   output_file = File.join(target_directory, new_filename(input_file))
   logger.debug("Writing sanitized HTML to: #{output_file}")
   File.write(output_file, sanitized_html)
+
+  logger.debug("Stripping leading lines")
+  strip_leading_lines(output_file, category(input_file))
+
+  logger.debug("Stripping trailing white space")
+  strip_trailing_white_space(output_file)
 end
