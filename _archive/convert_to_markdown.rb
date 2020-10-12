e9fe7e60333d4c451ec9a573d21f8fd78a548cc4
Dan Horst <danhorst@gmail.com>
2020-10-11 22:00:02 -0400

    Adding a preprocessor to the conversion process

--- _archive/convert_to_markdown.rb
+++ _archive/convert_to_markdown.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 
 require 'logger'
-require 'reverse_markdown'
+require 'kramdown'
 
 def logger
   @logger ||= Logger.new(STDOUT)
@@ -11,16 +11,23 @@ working_directory = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 logger.debug(working_directory)
 Dir.chdir working_directory
 
-html_files = `git ls-files | grep -v '_archive' | grep 'html$'`
+html_files = `git ls-files | grep -v '_archive' | grep 'html$'`.split(/\n/)
 
-html_files.split(/\n/).each do |html_file|
+
+html_files.each do |html_file|
   directory = File.dirname(html_file)
   basename = File.basename(html_file, '.*')
   markdown_file = "#{File.join(directory, basename)}.md"
-  cmd = "reverse_markdown #{html_file} > #{markdown_file}"
+
+  logger.info("Treating #{html_file} as if it were markdown to preserve white space and writing to HTML")
+  raw_html = File.read(html_file)
+  File.write(html_file, Kramdown::Document.new(raw_html).to_html)
+
   logger.info("Converting: #{html_file} to markdown")
-  logger.debug(cmd)
-  `#{cmd}`
+  to_markdown_cmd = "reverse_markdown #{html_file} > #{markdown_file}"
+  logger.debug(to_markdown_cmd)
+  `#{to_markdown_cmd}`
+
   logger.info("Removing: #{html_file}")
-  `git rm #{html_file}`
+  `git rm -f #{html_file}`
 end
